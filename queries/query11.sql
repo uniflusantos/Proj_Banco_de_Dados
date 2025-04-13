8ac5027ff9afea907d0c9d3c87d147bbff5727ae
uniflusantos <146308610+uniflusantos@users.noreply.github.com>
2025-04-13 19:52:05 -0300

    Update query11.sql

--- queries/query11.sql
+++ queries/query11.sql
@@ -1,4 +1,4 @@
--- Query: Liste os cursos que foram ministrados pelos professores 'PD4539' e 'PC2188'
+-- Query: Liste os cursos que foram ministrados pelos professores 'PD6150' e 'PC5833'
 
 SELECT DISTINCT 
     c."Curso_ID",
@@ -13,5 +13,5 @@ JOIN "Professor" p ON hp."Prof_ID" = p."Prof_ID"
 JOIN "Disciplina" d ON hp."Disc_ID" = d."Disc_ID"
 JOIN "Matriz_Curricular" mc ON d."Disc_ID" = mc."Disc_ID"
 JOIN "Curso" c ON mc."Curso_ID" = c."Curso_ID"
-WHERE hp."Prof_ID" IN ('PD4539', 'PC2188')
+WHERE hp."Prof_ID" IN ('PD6150', 'PC5833')
 ORDER BY c."Nome", d."Nome", p."Nome", hp."Ano", hp."Semestre";
