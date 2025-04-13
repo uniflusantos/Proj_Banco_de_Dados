634e63c43d346bc5eef27159d163686fb620ed1c
uniflusantos <146308610+uniflusantos@users.noreply.github.com>
2025-04-13 20:09:37 -0300

    Update query7.sql

--- queries/query7.sql
+++ queries/query7.sql
@@ -1,4 +1,4 @@
--- Encontra os professores que ensinam "Digital Experience" no primeiro semestre de 2025
+-- Encontra os professores que ensinam "Orientação a Objetos" no segundo semestre de 2023
 SELECT
     p."Prof_ID" AS id_professor,
     p."Nome" AS nome_professor,
@@ -12,8 +12,8 @@ FROM
     JOIN "Historico_Professor" hp ON p."Prof_ID" = hp."Prof_ID"
     JOIN "Disciplina" d ON hp."Disc_ID" = d."Disc_ID"
 WHERE
-    d."Disc_ID" = 'CD9785'
-    AND hp."Semestre" = 'Primeiro'
-    AND hp."Ano" = '2025'
+    d."Disc_ID" = 'CC6753'
+    AND hp."Semestre" = 'Segundo'
+    AND hp."Ano" = '2023'
 ORDER BY
     p."Nome";
