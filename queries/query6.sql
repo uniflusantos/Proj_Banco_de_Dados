d507f75451f6a868c0b138e53a6c55d8853467fd
uniflusantos <146308610+uniflusantos@users.noreply.github.com>
2025-04-13 18:04:55 -0300

    Update query6.sql

--- queries/query6.sql
+++ queries/query6.sql
@@ -1,4 +1,24 @@
--- Query: Encontre os nomes de todos os estudantes
+-- Encontra os estudantes que cursaram "Engenharia de Software" e "Redes de Computadores" no mesmo semestre (AND)
+SELECT 
+    a."Nome" AS nome_aluno,
+    a."RA" AS ra_aluno,
+    h1."Semestre" AS semestre,
+    h1."Ano" AS ano
+FROM 
+    "Aluno" a
+JOIN 
+    "Historico_Aluno" h1 ON a."RA" = h1."RA"
+JOIN 
+    "Historico_Aluno" h2 ON a."RA" = h2."RA" 
+    AND h1."Semestre" = h2."Semestre" 
+    AND h1."Ano" = h2."Ano"
+WHERE 
+    h1."Disc_Nome" = 'Fundamentos de Algoritmos'
+    AND h2."Disc_Nome" = 'Cálculo I'
+ORDER BY 
+    a."Nome", 
+    h1."Ano", 
+    h1."Semestre";-- Query: Encontre os nomes de todos os estudantes
 SELECT 
     "RA",
     "Nome"
