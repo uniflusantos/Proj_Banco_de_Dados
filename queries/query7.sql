814d01bcd1719a884fe35390379d8f9ef359640c
uniflusantos <146308610+uniflusantos@users.noreply.github.com>
2025-04-13 18:15:18 -0300

    Update query7.sql

--- queries/query7.sql
+++ queries/query7.sql
@@ -1,6 +1,19 @@
--- Query: Liste os IDs e nomes de todos os professores
-SELECT 
-    "Prof_ID",
-    "Nome"
-FROM "Professor"
-ORDER BY "Nome";
+-- Encontra os professores que ensinam "Digital Experience" no primeiro semestre de 2025
+SELECT
+    p."Prof_ID" AS id_professor,
+    p."Nome" AS nome_professor,
+    p."Departamento" AS departamento,
+    d."Disc_ID" AS id_disciplina,
+    d."Nome" AS nome_disciplina,
+    hp."Semestre" AS semestre,
+    hp."Ano" AS ano
+FROM
+    "Professor" p
+    JOIN "Historico_Professor" hp ON p."Prof_ID" = hp."Prof_ID"
+    JOIN "Disciplina" d ON hp."Disc_ID" = d."Disc_ID"
+WHERE
+    d."Disc_ID" = 'CD9785'
+    AND hp."Semestre" = 'Primeiro'
+    AND hp."Ano" = '2025'
+ORDER BY
+    p."Nome";
