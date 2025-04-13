acefd0f53419842c8e0f559ca3decc26dfa73360
uniflusantos <146308610+uniflusantos@users.noreply.github.com>
2025-04-13 20:12:58 -0300

    Update query9.sql

--- queries/query9.sql
+++ queries/query9.sql
@@ -1,8 +1,7 @@
--- Query: Encontre os nomes de todos os estudantes que cursaram "Ciência de Dados" (curso_id = 'C98')
-
+-- Query: Encontre os nomes de todos os estudantes que cursaram "Ciência de Dados" 
 SELECT DISTINCT 
     a."RA",
     a."Nome"
 FROM "Aluno" a
-WHERE a."Curso" = 'C98'
+WHERE a."Curso" = 'C42'
 ORDER BY a."Nome";
