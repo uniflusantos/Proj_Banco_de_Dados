ba9a6c2415ffcafc7deef89bb801af0790c41dfa
uniflusantos <146308610+uniflusantos@users.noreply.github.com>
2025-04-13 19:55:57 -0300

    Update query13.sql

--- queries/query13.sql
+++ queries/query13.sql
@@ -1,4 +1,4 @@
--- Query: Liste os cursos que são ministrados pelo professor 'PC8326', juntamente com os títulos dos cursos
+-- Query: Liste os cursos que são ministrados pelo professor 'PC5963', juntamente com os títulos dos cursos
 
 SELECT DISTINCT 
     c."Curso_ID",
@@ -11,5 +11,6 @@ FROM "Historico_Professor" hp
 JOIN "Disciplina" d ON hp."Disc_ID" = d."Disc_ID"
 JOIN "Matriz_Curricular" mc ON d."Disc_ID" = mc."Disc_ID"
 JOIN "Curso" c ON mc."Curso_ID" = c."Curso_ID"
-WHERE hp."Prof_ID" = 'PC8326'
+WHERE hp."Prof_ID" = 'PC5963'
+    AND hp."Ano" = '2025'
 ORDER BY c."Nome", hp."Ano", hp."Semestre", d."Nome";
